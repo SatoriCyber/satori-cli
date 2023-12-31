909140ace3c353d2f4e144a632cc5fb248f1ae5a
omer-shtivi <85064354+omer-shtivi@users.noreply.github.com>
2023-12-31 11:30:32 +0200

    Anyhow for errors (#62)
    * Anyhow for errors
    
    * amend

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use cli::parser::parse;
+use anyhow::{anyhow, Ok, Result};
+use cli::parser::{parse, Flow};
 use helpers::logger;
 
 use crate::cli::auto_complete;
@@ -15,35 +16,31 @@ async fn main() {
     let cli_results = parse();
     logger::init(cli_results.debug);
     log::debug!("running satori cli with parameters: {:?}", cli_results);
-    let mut exit_status = 0;
-    match cli_results.flow {
-        cli::parser::Flow::Login(params) => {
-            if let Err(err) = login::run(&params).await {
-                log::error!("Failed to login: {}", err);
-                exit_status = 1;
-            };
-        }
-        cli::parser::Flow::Connect(params) => {
-            if let Err(err) = connect::run(params).await {
-                log::error!("{}", err);
-                exit_status = 1;
-            }
-        }
-        cli::parser::Flow::AutoComplete(params, out) => {
-            auto_complete::run(params, out);
-        }
-        cli::parser::Flow::List(params) => {
-            if let Err(err) = list::run(params) {
-                log::error!("{}", err);
-                exit_status = 1;
-            }
-        }
-        cli::parser::Flow::Tools(params) => {
-            if let Err(err) = tools::run(params).await {
-                log::error!("{}", err);
-                exit_status = 1;
-            }
-        }
+
+    let exit_status = if let Err(err) = handle_flow(cli_results.flow).await {
+        log::error!("{}", err);
+        1
+    } else {
+        0
     };
     std::process::exit(exit_status);
+}
+
+async fn handle_flow(flow: Flow) -> Result<()> {
+    match flow {
+        cli::parser::Flow::Login(params) => login::run(&params)
+            .await
+            .map_err(|err| anyhow!("Failed to login: {}", err)),
+        cli::parser::Flow::Connect(params) => connect::run(params)
+            .await
+            .map_err(|err| anyhow!("Failed to connect: {}", err)),
+        cli::parser::Flow::AutoComplete(params, out) => {
+            auto_complete::run(params, out);
+            Ok(())
+        }
+        cli::parser::Flow::List(params) => list::run(params).map_err(|err| anyhow!("{}", err)),
+        cli::parser::Flow::Tools(params) => {
+            tools::run(params).await.map_err(|err| anyhow!("{}", err))
+        }
+    }
 }
