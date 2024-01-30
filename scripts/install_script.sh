5390c88a5142772a2f72542beccea7d6afb042c6
Omer Shtivi <omer.shtivi@satoricyber.com>
2024-01-30 13:43:34 +0200

    version 0.0.13

--- scripts/install_script.sh
+++ scripts/install_script.sh
@@ -2,7 +2,7 @@
 
 # Define variables
 APP_NAME="satori"
-LATEST_APP_VERSION="0.0.12"
+LATEST_APP_VERSION="0.0.13"
 DOWNLOAD_URL="https://github.com/SatoriCyber/satori-cli/releases/download/v$LATEST_APP_VERSION/$APP_NAME-$LATEST_APP_VERSION-linux.tar.gz"
 
 # Check if the script is run with root privileges
