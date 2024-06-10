f7a018ea17511085b6ed6e829a7f09484d12c411
Omer Shtivi <omer.shtivi@satoricyber.com>
2024-06-10 11:48:46 +0300

    chore: Release satori-cli version 0.0.27

--- scripts/install_script.sh
+++ scripts/install_script.sh
@@ -2,7 +2,7 @@
 
 # Define variables
 APP_NAME="satori"
-LATEST_APP_VERSION="0.0.26"
+LATEST_APP_VERSION="0.0.27"
 DOWNLOAD_URL_PREFIX="https://github.com/SatoriCyber/satori-cli/releases/download/v$LATEST_APP_VERSION/$APP_NAME-$LATEST_APP_VERSION"
 
 HAS_CURL="$(type "curl" &> /dev/null && echo true || echo false)"
